fb91e480264d0659c42e8135dbad2e590167a1ff
rodrigo <omirrp28@hotmail.com>
2017-12-06 15:07:55 -0600

    ChainOfCommandTerminado
    Chain of Command terminado

---
+++ PanelProfesores/PanelProfesores/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanelProfesores
+{
+    class Alumno
+    {
+        public int id { get; set; }
+        public float calificacion { get; set; }
+        
+    }
+}
